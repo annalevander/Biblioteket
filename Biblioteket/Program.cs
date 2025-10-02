0d27b593d7196425d3d64061afce749441f2412f
annalevander <annalevander@icloud.com>
2025-10-02 17:05:41 +0200

    created a start menu and the start of a user system method

--- Biblioteket/Program.cs
+++ Biblioteket/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            
+            StartMenu();
+            UserSystem();
+        }
+
+        static void StartMenu()
+        {
+            Console.WriteLine("Välkommen till lånecentralen");
+            Console.Write("Användarnamn:");
+            Console.ReadLine();
+            Console.Write("PIN:");
+            Console.ReadLine();
+        }
+
+        static void UserSystem()
+        {
+            string[] UserOne = ["Anna", "1234"];
+            string[] UserTwo = ["Cajsa", "1235"];
+            string[] UserThree = ["Kitang", "1236"];
+            string[] UserFour = ["Oscar", "1237"];
+            string[] UserFive = ["TregNeko", "1238"];
+            string[][] Users = [UserOne, UserTwo, UserThree, UserFour, UserFive];
         }
     }
 }
